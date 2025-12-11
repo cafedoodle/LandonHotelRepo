c97d437e34fb8b091829ce19ec51dd11b9dc3c09
Doodle <23706830+cafedoodle@users.noreply.github.com>
2025-12-11 12:18:25 -0600

    Initial commit with LandonHotel and LandonHotel.Tests

---
+++ LandonHotel.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LandonHotel.Tests
+{
+   
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+
+        //[Fact]
+        //public void Test(int a)
+        //{
+        //}
+    }
+}
